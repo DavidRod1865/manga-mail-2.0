53a449704f517b9c8040df8c976a61d1de1f6b43
David Rodriguez <David.Rod1865@gmail.com>
2023-02-01 23:41:10 -0500

    Added Profit Calculation

--- src/js/script.js
+++ src/js/script.js
@@ -8,39 +8,55 @@ const includingFees = document.querySelector('.includingFees')
 class Books {
     constructor(weight, price) {
         // console.log('this', this)
-        this.weight = weight;
-        this.price = (price).toFixed(2);
+        this.weight = Number(weight);
+        this.price = Number((price).toFixed(2));
+
+        //Calculates "USPS Shipping Rate" using Custom Media Mail API
+        this.calShipping = async () => {
+            await fetch(`https://media-mail-api.vercel.app/api/weight/${weight}`)
+                .then(res => res.json())
+                .then(data => { 
+                    shippingFee.innerText = Number(data)
+                    return data
+                })
+                .catch(err => alert(`Weight must be less than 70 Lbs.`))
+            
+        };
+        
+        /*Calculates "Paypal Commission Fee" using calculation found at:
+        https://www.paypal.com/us/webapps/mpp/merchant-fees*/
+        this.calPayPal =  async () => {
+            if (Number(price) > 0){
+                let fee = Number(((price * .0349) + 0.49).toFixed(2))
+                paypalFee.innerText = fee
+                return fee
+            }   
+            else if (Number(price) <= 0) {
+                paypalFee.innerText = "0.00"
+                return 0
+            }
+        }
     }
-    calShipping = async () => {
-        // console.log(`Wow, this package weighs ${this.weight}.`)
-        await fetch(`https://media-mail-api.vercel.app/api/weight/${this.weight}`)
-            .then(res => res.json())
-            .then(data => shippingFee.innerText = Number(data))
-            .catch(err => alert(`Weight must be less than 70 Lbs.`))
+
+    calProfit = async () => {
+    let shipping = await this.calShipping();
+    let paypal = await this.calPayPal();
+    console.log(Number(shipping), typeof shipping)
+        excludingFees.innerText = Number(this.price - (shipping + paypal));
+        includingFees.innerText = Number(this.price + (paypal))
     };
 
-    calPayPal = () => {
-        console.log(`Wow, this package costs ${this.price}.`)
-        if (this.price > 0){
-            paypalFee.innerText = ((this.price * .0349) + 0.49).toFixed(2)
-        }   
-        else paypalFee.innerText = `0.00`
-    }
+};
 
-    calProfit = () => {
-        excludingFees.innerText = this.price - (this.calShipping + this.calPayPal)
-        includingFees.innerText = this.price + (this.calShipping + this.calPayPal)
-    }
-
-}
-
-function getValues() {
+async function getValues() {
     let weight = +document.querySelector(".weight").value;
     let price = +document.querySelector(".price").value;
     const packaged_Books = new Books(weight, price);
-    packaged_Books.calShipping();
-    packaged_Books.calPayPal();
+    await packaged_Books.calShipping();
+    await packaged_Books.calPayPal();
+    await packaged_Books.calProfit();
 }
+
 
 function appear() {
     if ($('div').hide()) $('div').show();
