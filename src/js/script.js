d724d9d7babe0321bb9f22ea95c3b439680daaa5
David Rodriguez <David.Rod1865@gmail.com>
2023-02-06 22:37:54 -0500

    Updated CSS and Updated Formula for Displaying the Fees

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@ const shippingFee = document.querySelector('.shipping')
 const paypalFee = document.querySelector('.paypal')
 const excludingFees = document.querySelector('.excludingFees')
 const includingFees = document.querySelector('.includingFees')
+const weightValue = document.querySelector('.weightValue')
+const valueValue = document.querySelector('.valueValue')
 
 class Books {
     constructor(weight, price) {
@@ -15,7 +17,9 @@ class Books {
                 let uspsRate = +(0.7 * (weight - 1) + 3.65).toFixed(2)
                 shippingFee.innerText = uspsRate
                 return uspsRate
-            } else alert(`Weight must be less than 70 Lbs.`)
+            } else 
+                alert(`Weight must be less than 70 Lbs.`)
+                location.reload()
         };
         
         //Calculates "Paypal Commission Fee"
@@ -34,6 +38,8 @@ class Books {
     
     //Calculates the User's profit before & after taxes
     calProfit = () => {
+        weightValue.innerText = (+document.querySelector(".weight").value).toFixed(2);
+        valueValue.innerText = (+document.querySelector(".price").value).toFixed(2);
         excludingFees.innerText = (this.price - (this.calShipping() + this.calPayPal())).toFixed(2);
         includingFees.innerText = (this.price + (this.calShipping() + this.calPayPal())).toFixed(2);
     };
